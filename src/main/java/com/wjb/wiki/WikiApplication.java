b333b0ddaefb7889c0de03a25a5601598afb90dd
wangjianbing <wangjianbin003@chinasoftinc.com>
2021-11-30 14:56:58 +0800

    2021

--- src/main/java/com/wjb/wiki/WikiApplication.java
+++ src/main/java/com/wjb/wiki/WikiApplication.java
@@ -1,14 +1,20 @@
 package com.wjb.wiki;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+/**
+ * @author Administrator
+ */
 @SpringBootApplication
 public class WikiApplication {
-
-
+    private static final Logger LOG=LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app=new SpringApplication(WikiApplication.class);
+        Environment environment= app.run(args).getEnvironment();
+        LOG.info("启动成功");
     }
 
 }
